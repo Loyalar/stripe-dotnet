c26074a41a1588d95664f888c5ac937d034b8ba6
CJ Avilla <cjavilla@stripe.com>
2020-06-17 07:23:33 -0700

    Remove comments

--- src/Stripe.net/Entities/SubscriptionSchedules/SubscriptionScheduleDefaultSettings.cs
+++ src/Stripe.net/Entities/SubscriptionSchedules/SubscriptionScheduleDefaultSettings.cs
@@ -5,28 +5,14 @@ namespace Stripe
 
     public class SubscriptionScheduleDefaultSettings : StripeEntity<SubscriptionScheduleDefaultSettings>
     {
-        /// <summary>
-        /// Define thresholds at which an invoice will be sent, and the subscription advanced to a
-        /// new billing period.
-        /// </summary>
         [JsonProperty("billing_thresholds")]
         public SubscriptionBillingThresholds BillingThresholds { get; set; }
 
-        /// <summary>
-        /// Either <c>charge_automatically</c>, or <c>send_invoice</c>. When charging
-        /// automatically, Stripe will attempt to pay this subscription at the
-        /// end of the cycle using the default source attached to the customer.
-        /// When sending an invoice, Stripe will email your customer an invoice
-        /// with payment instructions.
-        /// </summary>
         [JsonProperty("collection_method")]
         public string CollectionMethod { get; set; }
 
         #region Expandable DefaultPaymentMethod
 
-        /// <summary>
-        /// ID of the default payment method for the subscription schedule.
-        /// </summary>
         [JsonIgnore]
         public string DefaultPaymentMethodId
         {
@@ -46,18 +32,9 @@ namespace Stripe
         internal ExpandableField<PaymentMethod> InternalDefaultPaymentMethod { get; set; }
         #endregion
 
-        /// <summary>
-        /// The schedule's default invoice settings.
-        /// </summary>
         [JsonProperty("invoice_settings")]
         public SubscriptionScheduleInvoiceSettings InvoiceSettings { get; set; }
 
-        /// <summary>
-        /// The account (if any) the subscription's payments will be
-        /// attributed to for tax reporting, and where funds from each
-        /// payment will be transferred to for each of the subscription's
-        /// invoices.
-        /// </summary>
         [JsonProperty("transfer_data")]
         public SubscriptionTransferData TransferData { get; set; }
     }
